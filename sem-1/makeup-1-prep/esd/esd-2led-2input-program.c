4a1bdb2ac069826a12d32d23f52a0da65726c78c
Anu Shibin Joseph Raj <ajosephr@opentext.com>
2022-10-07 17:06:53 +0530

    PIN0 can select only 16 bits. Updated comments accordingly. Code doesn't change.

--- sem-1/makeup-1-prep/esd/esd-2led-2input-program.c
+++ sem-1/makeup-1-prep/esd/esd-2led-2input-program.c
@@ -15,32 +15,32 @@
 int main()
 {
     /* Configuring P0.0 to P0.15 as GPIO */
-    /* 00000000000000000000000000000000 (Bin) = 0x0 (Hex) */
+    /* 0000000000000000 (Bin) = 0x0 (Hex) */
     PINSEL0 = 0x0;
 
     /* SWITCH1(P0.0) and SWITCH2(P0.1) bit as an input pin */
-    /* 00000000000000000000000000000011 (Bin) = 0x3 (Hex) */
+    /* 0000000000000011 (Bin) = 0x3 (Hex) */
     IODIR0 = 0x3;
 
     while (1)
     {
         /* If SWITCH1(P0.0) is closed & pressed, pin is LOW */
-        /* 00000000000000000000000000000001 (Bin) = 0x1 (Hex) */
+        /* 0000000000000001 (Bin) = 0x1 (Hex) */
         if (!(IOPIN0 & 0x1))
         {
             /* Turn on LED1(P0.4) & LED2(P0.5) */
             /* Also, unpress SWITCH1(P0.0) to avoid 2 switches being pressed at the same time*/
-            /* 00000000000000000000000000110001 (Bin) = 0x31 (Hex) */
+            /* 0000000000110001 (Bin) = 0x31 (Hex) */
             IOSET0 = 0x31;
         }
 
         /* If SWITCH1(P0.1) is closed & pressed, pin is LOW */
-        /* 00000000000000000000000000000010 (Bin) = 0x2 (Hex) */
+        /* 0000000000000010 (Bin) = 0x2 (Hex) */
         if (!(IOPIN0 & 0x2))
         {
             /* Turn off LED1(P0.4) & LED2(P0.5) */
             /* Also, unpress SWITCH2(P0.1) to avoid 2 switches being pressed at the same time*/
-            /* 00000000000000000000000000110010 (Bin) = 0x32 (Hex) */
+            /* 0000000000110010 (Bin) = 0x32 (Hex) */
             IOCLR0 = 0x32;
         }
     }
